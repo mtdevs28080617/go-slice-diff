b6ab9462c7959ae5c487b127073f3af4a319e9c2
Matthew Thomas <matthew.thomas@mtdevs.co.uk>
2024-10-03 22:19:28 +0100

    remove unused interface for contraints

--- slice_diff.go
+++ slice_diff.go
@@ -2,11 +2,6 @@ package slice_diff
 
 import "golang.org/x/exp/constraints"
 
-// Diff interface describes the types that SliceDiff can check against
-type Diff interface {
-	string | int | int8 | int16 | int32 | int64 | float32 | float64
-}
-
 // SliceDiff takes 2 slices of the same type and returns the difference
 // The difference returned is what listB does not match within listA
 func SliceDiff[T constraints.Ordered](listA []T, listB []T) []T {
